246edc958b8dd9ab48da8076470b9d381c2b46e0
Harley McDowell <hmcdowell95@gmail.com>
2020-04-29 04:47:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ module FormsLab
     end
     
     get '/new' do 
+<<<<<<< HEAD
       erb :"pirates/new"
+=======
+      erb :new
+>>>>>>> feb4f01e41a68a944aaa62413dcdbccb3c27013f
     end
     
     post '/pirates' do 
@@ -17,7 +21,11 @@ module FormsLab
         Ship.new(ship)
       end
       @ships = Ship.all
+<<<<<<< HEAD
       erb :"pirates/show"
+=======
+      erb :show
+>>>>>>> feb4f01e41a68a944aaa62413dcdbccb3c27013f
     end
         
 
